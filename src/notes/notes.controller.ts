4af5c8b53571b4a6faeb9c8cb6fac214af3e9a82
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-08-06 16:01:49 +0300

    added controller

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { SchemaValidationPipe } from 'src/common/pipes/schema-validation/schema-validation.pipe';
+import { createNoteSchema } from './common/schemas/create-note.schema';
+import { UpdateNoteSchema } from './common/schemas/update-note.schema';
 
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
   @Post()
+  @UsePipes(new SchemaValidationPipe(createNoteSchema))
   create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
+    this.notesService.create(createNoteDto);
   }
 
   @Get()
   findAll() {
     return this.notesService.findAll();
   }
-
+  @Get('/stats')
+  stats() {
+    return this.notesService.stats();
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
+    return this.notesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
+  update(
+    @Param('id') id: string,
+    @Body(new SchemaValidationPipe(UpdateNoteSchema))
+    updateNoteDto: UpdateNoteDto,
+  ) {
+    return this.notesService.update(id, updateNoteDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.notesService.remove(+id);
+    return this.notesService.remove(id);
   }
 }
